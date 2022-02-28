a72a6b25c655272c4202ffbd8d255108fff00945
cjpwq <50103094+cjpwq@users.noreply.github.com>
2022-02-28 15:31:45 +0800

    New updates

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,63 @@
 <template>
-  <div class="docker">
-    <div class="docker__item docker__item--active">
-      <div class="iconfont">&#xe6f3;</div>
-      <div class="docker__title">首页</div>
-    </div>
-    <div class="docker__item">
-      <div class="iconfont">&#xe7e5;</div>
-      <div class="docker__title">购物车</div>
+  <div>
+    <div class="wrapper">
+      <div class="position">
+        <span class="iconfont position__icon">&#xe619;</span>
+        北京理工大学国防科技园2号楼10层
+        <span class="iconfont position_notice">&#xe60b;</span>
       </div>
-    <div class="docker__item">
-      <div class="iconfont">&#xe61e;</div>
-      <div class="docker__title">订单</div>
     </div>
-    <div class="docker__item">
-      <div class="iconfont">&#xe660;</div>
-      <div class="docker__title">我的</div>
+    <div class="docker">
+      <div class="docker__item docker__item--active">
+        <div class="iconfont">&#xe6f3;</div>
+        <div class="docker__title">首页</div>
+      </div>
+      <div class="docker__item">
+        <div class="iconfont">&#xe7e5;</div>
+        <div class="docker__title">购物车</div>
+        </div>
+      <div class="docker__item">
+        <div class="iconfont">&#xe61e;</div>
+        <div class="docker__title">订单</div>
+      </div>
+      <div class="docker__item">
+        <div class="iconfont">&#xe660;</div>
+        <div class="docker__title">我的</div>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+@import './style/viriables.scss';
+@import './style/mixins.scss';
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right: 0;
+  padding: 0 .18rem;
+}
+.position {
+  position: relative;
+  padding: .16rem .24rem .16rem 0;
+  line-height: .22rem;
+  font-size: .16rem;
+  @include ellipsis;
+  .position__icon {
+    position: relative;
+    top: .01rem;
+    font-size: .2rem;
+  }
+  .position_notice {
+    position: absolute;
+    right: 0;
+    top: .17rem;
+    font-size: .2rem;
+  }
+  color: $content-fontcolor;
+}
 .docker {
   display: flex;
   box-sizing: border-box;
@@ -29,7 +67,8 @@
   bottom: 0;
   width: 100%;
   height: .49rem;
-  border-top: 1px solid #F1F1F1;
+  border-top: .01rem solid #F1F1F1;
+  color: $content-fontcolor;
   &__item {
     flex: 1;
     text-align: center;
@@ -42,7 +81,7 @@
     }
   }
   &__title {
-    font-size: 20px;
+    font-size: .2rem;
     transform: scale(.5, .5);
     transform-origin: center top;
   }
